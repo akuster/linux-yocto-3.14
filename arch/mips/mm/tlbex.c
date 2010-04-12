9928e87bee05724a195fe191d3c90e3813df3cd9
Weixing Shi <weixing.shi@windriver.com>
2010-04-12 14:59:38 +0800

    4kc cache tlb hazard: tlbp cache coherency
    Add a no-op before tlbp, to avoid DMA cache coherency issues that cause
    data loss seen on unmount.
    
    Signed-off-by: <weixing.shi@windriver.com> Weixing Shi
    Signed-off-by: <james.perkins@windriver.com> James Perkins

--- arch/mips/mm/tlbex.c
+++ arch/mips/mm/tlbex.c
@@ -473,6 +473,7 @@ static void __maybe_unused build_tlb_probe_entry(u32 **p)
 	case CPU_R4700:
 	case CPU_R5000:
 	case CPU_NEVADA:
+	case CPU_4KC:
 		uasm_i_nop(p);
 		uasm_i_tlbp(p);
 		break;
@@ -540,6 +541,7 @@ static void build_tlb_write_entry(u32 **p, struct uasm_label **l,
 
 	case CPU_R4600:
 	case CPU_R4700:
+	case CPU_4KC:
 		uasm_i_nop(p);
 		tlbw(p);
 		uasm_i_nop(p);
@@ -564,7 +566,6 @@ static void build_tlb_write_entry(u32 **p, struct uasm_label **l,
 	case CPU_R10000:
 	case CPU_R12000:
 	case CPU_R14000:
-	case CPU_4KC:
 	case CPU_4KEC:
 	case CPU_M14KC:
 	case CPU_M14KEC:
