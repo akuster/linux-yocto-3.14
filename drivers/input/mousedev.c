961be119337e6e4022cd49a2481a4b9bd68a53e6
Richard Griffiths <richard.griffiths@windriver.com>
2010-09-21 13:22:55 -0700

    mousedev: mousedev patch for qemumips
    The consumers of the mousedev data in MIPS uperspace assume
    that the data is little endian. So if we are running on a
    big endian CPU we force the output data to be little endian.
    
    Note: this may be reverted if/when the consuming devices
    add options, or options are found that can byte swap the
    data in userspace. This is simply a commit of convienience
    to demonstrate an issue.
    
    Signed-off-by: Richard Griffiths <richard.griffiths@windriver.com>
    Signed-off-by: Bruce Ashfield <bruce.ashfield@windriver.com>

--- drivers/input/mousedev.c
+++ drivers/input/mousedev.c
@@ -26,6 +26,7 @@
 #include <linux/device.h>
 #include <linux/cdev.h>
 #include <linux/kernel.h>
+#include <asm/byteorder.h>
 
 MODULE_AUTHOR("Vojtech Pavlik <vojtech@ucw.cz>");
 MODULE_DESCRIPTION("Mouse (ExplorerPS/2) device interfaces");
@@ -736,6 +737,11 @@ static ssize_t mousedev_read(struct file *file, char __user *buffer,
 	memcpy(data, client->ps2 + client->bufsiz - client->buffer, count);
 	client->buffer -= count;
 
+#ifdef CONFIG_CPU_BIG_ENDIAN
+	/* Force mouse data LE in userspace as consumers
+	   of the data expect it in this format */
+	cpu_to_le16p(data);
+#endif
 	spin_unlock_irq(&client->packet_lock);
 
 	if (copy_to_user(buffer, data, count))
