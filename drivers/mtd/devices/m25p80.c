8f30c95869fdc97526d0df4b9776553a8c9506cb
nbd <nbd@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2012-07-17 11:35:05 -0400

    476-mtd-m25p80-allow-to-disable-small-sector-erase
    Import of the above patch from openwrt trunk, as of this commit:
    
     ----------
    commit c1d79f64eed0a7ac36b5b9bca52275b397bec424
    Author: nbd <nbd@3c298f89-4303-0410-b956-a3cf2f4a3e73>
    Date:   Mon Jul 16 16:26:51 2012 +0000
    
        uboot-ar71xx: fix compile on recent mac os x versions
    
        git-svn-id: svn://svn.openwrt.org/openwrt/trunk@32750 3c298f89-4303-0410-b956-a3cf2f4a3e73
     ----------
    
    Path to patch in the repo is: target/linux/generic/patches-3.3
    Repo is: git://nbd.name/openwrt.git
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/mtd/devices/m25p80.c
+++ drivers/mtd/devices/m25p80.c
@@ -91,6 +91,12 @@
 
 #define JEDEC_MFR(_jedec_id)	((_jedec_id) >> 16)
 
+#ifdef CONFIG_M25PXX_PREFER_SMALL_SECTOR_ERASE
+#define PREFER_SMALL_SECTOR_ERASE 1
+#else
+#define PREFER_SMALL_SECTOR_ERASE 0
+#endif
+
 /****************************************************************************/
 
 enum read_type {
@@ -1194,7 +1200,7 @@ static int m25p_probe(struct spi_device *spi)
 		flash->mtd._write = m25p80_write;
 
 	/* prefer "small sector" erase if possible */
-	if (info->flags & SECT_4K) {
+	if (PREFER_SMALL_SECTOR_ERASE && (info->flags & SECT_4K)) {
 		flash->erase_opcode = OPCODE_BE_4K;
 		flash->mtd.erasesize = 4096;
 	} else if (info->flags & SECT_4K_PMC) {
