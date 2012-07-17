0ee389358bb5d968c1ae2a1eef9cef9ea69ca8d8
nbd <nbd@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2012-07-17 11:35:03 -0400

    440-block2mtd_init
    Import of the above patch from openwrt trunk, as of this commit:
    
     ----------
    commit c1d79f64eed0a7ac36b5b9bca52275b397bec424
    Author: nbd <nbd@3c298f89-4303-0410-b956-a3cf2f4a3e73>
    Date:   Mon Jul 16 16:26:51 2012 +0000
    
        uboot-ar71xx: fix compile on recent mac os x versions
    
        git-svn-id: svn://svn.openwrt.org/openwrt/trunk@32750 3c298f89-4303-0410-b956-a3cf2f4a3e73
     ----------
    
    Path to patch in the repo is: target/linux/generic/patches-3.3
    Repo is: git://nbd.name/openwrt.git
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- block/partition-generic.c
+++ block/partition-generic.c
@@ -548,6 +548,7 @@ int invalidate_partitions(struct gendisk *disk, struct block_device *bdev)
 
 	return 0;
 }
+EXPORT_SYMBOL(rescan_partitions);
 
 unsigned char *read_dev_sector(struct block_device *bdev, sector_t n, Sector *p)
 {
