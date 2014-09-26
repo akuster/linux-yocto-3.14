830931d7bce8fdecd07838afc0e058d07c59a5db
Bruce Ashfield <bruce.ashfield@windriver.com>
2014-09-26 15:34:04 -0400

    Revert "net: filter: be more defensive on div/mod by X==0"
    This reverts commit 41bdf9a8c75f62d49138df4aaff09553b8675a29.

--- net/core/filter.c
+++ net/core/filter.c
@@ -295,43 +295,43 @@ select_insn:
 		(*(s64 *) &A) >>= K;
 		CONT;
 	BPF_ALU64_BPF_MOD_BPF_X:
-		if (unlikely(X == 0))
-			return 0;
 		tmp = A;
-		A = do_div(tmp, X);
+		if (X)
+			A = do_div(tmp, X);
 		CONT;
 	BPF_ALU_BPF_MOD_BPF_X:
-		if (unlikely(X == 0))
-			return 0;
 		tmp = (u32) A;
-		A = do_div(tmp, (u32) X);
+		if (X)
+			A = do_div(tmp, (u32) X);
 		CONT;
 	BPF_ALU64_BPF_MOD_BPF_K:
 		tmp = A;
-		A = do_div(tmp, K);
+		if (K)
+			A = do_div(tmp, K);
 		CONT;
 	BPF_ALU_BPF_MOD_BPF_K:
 		tmp = (u32) A;
-		A = do_div(tmp, (u32) K);
+		if (K)
+			A = do_div(tmp, (u32) K);
 		CONT;
 	BPF_ALU64_BPF_DIV_BPF_X:
-		if (unlikely(X == 0))
-			return 0;
-		do_div(A, X);
+		if (X)
+			do_div(A, X);
 		CONT;
 	BPF_ALU_BPF_DIV_BPF_X:
-		if (unlikely(X == 0))
-			return 0;
 		tmp = (u32) A;
-		do_div(tmp, (u32) X);
+		if (X)
+			do_div(tmp, (u32) X);
 		A = (u32) tmp;
 		CONT;
 	BPF_ALU64_BPF_DIV_BPF_K:
-		do_div(A, K);
+		if (K)
+			do_div(A, K);
 		CONT;
 	BPF_ALU_BPF_DIV_BPF_K:
 		tmp = (u32) A;
-		do_div(tmp, (u32) K);
+		if (K)
+			do_div(tmp, (u32) K);
 		A = (u32) tmp;
 		CONT;
 	BPF_ALU_BPF_END_BPF_TO_BE:
